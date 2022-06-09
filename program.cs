de145c113c24de690c3b7667184ea81b0891082a
Nirmal <nirmallimbu106@gmail.com>
2022-06-09 16:58:20 +0545

    first commit

---
+++ program.cs
@@ -0,0 +1,16 @@
+using system;
+namespace class
+{
+    class car{
+        int row=5;
+        for(int i=1;i<=row){
+            for(int j=1;j<=i;j++){
+  Console.write("*");
+            }
+            Console.ReadLine();
+        }
+    }
+    
+    }
+    
+}
